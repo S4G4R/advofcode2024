860d48f010ef617e0c03e186ab54f23e4d43af1a
Sagar Vrajalal <sagarvrajalal@gmail.com>
2025-06-18 23:54:10 +0530

    fix: Do not read entire input into memory

--- src/advofcode2024.clj
+++ src/advofcode2024.clj
@@ -5,10 +5,10 @@
 (defn -main
   [& [day]]
   (println "Solving day" day)
-  (let [input (slurp (io/resource (str day "/input.txt")))
-        day-ns (symbol (str "advofcode2024." day ".core"))
+  (let [day-ns (symbol (str "advofcode2024." day ".core"))
         _ (require day-ns)
         {doc :doc} (meta (the-ns day-ns))
         day-var (resolve (symbol (str day-ns) "solve"))]
     (println doc)
-    (day-var input)))
+    (with-open [reader (io/reader (io/resource (str day "/input.txt")))]
+      (day-var reader))))
